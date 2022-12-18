a31b9b16233eb2217c87cf364d01b6e4ee7cd343
Rob Bassett <33643965+robbassett@users.noreply.github.com>
2022-12-18 14:51:41 +1100

    taking shape

---
+++ prototype_server/apps/app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import config
+from models import Plant
+from plotting import plot_trace
+import os
+from jinja2 import TemplateNotFound
+
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml") 
+
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
+
+@app.route("/plotting", methods=["POST", "GET"])
+def make_plot():
+    return plot_trace(request.args.get('data'))
+
+@app.route("/")
+def home():
+    plants = Plant.query.all()
+    return render_template("/landing/index.html", plants=plants)
+
+@app.route("/dashboard/")
+def dashboard():
+    return render_template("/home/dashboard.html")
+
+@app.route('/<template>')
+def route_template(template):
+
+    try:
+
+        if not template.endswith('.html'):
+            template += '.html'
+
+        # Detect the current page
+        segment = get_segment(request)
+
+        # Serve the file (if exists) from app/templates/home/FILE.html
+        return render_template("home/" + template, segment=segment)
+
+    except TemplateNotFound:
+        return render_template('home/page-404.html'), 404
+
+    except:
+        return render_template('home/page-500.html'), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
