2f366d1e8df3baf9d153b3f6d2a26a0698d4b642
Rob Bassett <33643965+robbassett@users.noreply.github.com>
2023-02-19 16:08:26 +1100

    looking better

--- prototype_server/apps/plotting.py
+++ prototype_server/apps/plotting.py
@@ -22,17 +22,24 @@ def get_trace(plant_name,measure_name='Soil Moisture',db=db):
 
     x = np.array([value.timestamp for value in values])
     y = np.array([value.value for value in values])
-    print(plant_name,measure_name)
-    print(y)
-    print()
+
+    measure = Measure.query.filter(Measure.measure_name == "Light").one_or_none()
+    measure_id = measure.measure_id
+
+    values = Value.query.filter(
+        (Value.plant_id == plant_id)&(Value.measure_id == measure_id)
+    ).all()
+
+    y2 = np.array([value.value for value in values])
 
     xend = x.max()+timedelta(minutes=5)
-    xstart = xend-timedelta(hours=24)
+    xstart = xend-timedelta(hours=48)
     tsel = list(np.where(x >= xstart)[0])
     x = x[tsel]
     y = y[tsel]
 
-    return json.dumps(list(y))
+    out = json.dumps([list(y),list(y2)])
+    return out
 
     tord = list(np.argsort(x))
     fig = go.Figure()
