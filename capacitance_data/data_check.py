1fb4a728e17942ae40b06d368d52c232798cbdb2
Rob Bassett <33643965+robbassett@users.noreply.github.com>
2023-03-20 21:10:15 +1100

    readme

--- capacitance_data/data_check.py
+++ capacitance_data/data_check.py
@@ -12,22 +12,23 @@ eric = pd.read_sql("SELECT timestamp,value,V.measure_id FROM Value AS V JOIN Pla
 lily = pd.read_sql("SELECT timestamp,value,V.measure_id FROM Value AS V JOIN Plant as P ON V.plant_id=P.plant_id JOIN Measure as M ON V.measure_id=M.measure_id\
     WHERE V.plant_id=2",engine)
 
-F = plt.figure()
-ax = F.add_subplot(111)
+F = plt.figure(figsize=(16,14))
+lbs = ['Soil Moisture %','Soil Moisture Raw','Light %']
 
-x = np.array([dt.datetime.strptime(_,"%Y-%m-%d %H:%M:%S.%f") for _ in eric[eric['measure_id'] == 1]['timestamp']])
-y = np.array(eric[eric['measure_id'] == 1]['value'])
-t = np.where(x > dt.datetime.strptime("2023-03-13","%Y-%m-%d"))[0]
-ax.plot(x[t],y[t])
+for i in [1,2,3]:
+    ax = F.add_subplot(3,1,i)
 
-j = np.argmin(y[t])
-i = np.argmax(y[t])
-print(x[t[j]])
-print(x[t[i]])
+    x = np.array([dt.datetime.strptime(_,"%Y-%m-%d %H:%M:%S.%f") for _ in eric[eric['measure_id'] == i]['timestamp']])
+    y = np.array(eric[eric['measure_id'] == i]['value'])
+    t = np.where(x > dt.datetime.strptime("2023-03-13","%Y-%m-%d"))[0]
+    ax.plot(x[t],y[t],label='Erik')
 
-x = np.array([dt.datetime.strptime(_,"%Y-%m-%d %H:%M:%S.%f") for _ in lily[lily['measure_id'] == 1]['timestamp']])
-y = np.array(lily[lily['measure_id'] == 1]['value'])
-t = np.where(x > dt.datetime.strptime("2023-03-13","%Y-%m-%d"))[0]
-ax.plot(x[t],y[t])
+    x = np.array([dt.datetime.strptime(_,"%Y-%m-%d %H:%M:%S.%f") for _ in lily[lily['measure_id'] == i]['timestamp']])
+    y = np.array(lily[lily['measure_id'] == i]['value'])
+    t = np.where(x > dt.datetime.strptime("2023-03-13","%Y-%m-%d"))[0]
+    ax.plot(x[t],y[t],label='Lily')
+    if i == 1:
+        ax.legend(loc='upper center',ncol=2)
+    ax.set_ylabel(lbs[i-1])
 
-plt.show()
+plt.savefig('data_check.png')
